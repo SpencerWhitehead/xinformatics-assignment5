24045456ca32ddc212d0d6721cb3b46df2a461c8
delosh653 <hdelossantos653@gmail.com>
2017-05-01 22:11:47 -0400

    Add files via upload
    updated version of R backend

--- Xinformatics_Project_Team5_Backend.R
+++ Xinformatics_Project_Team5_Backend.R
@@ -17,77 +17,94 @@ library(broom)
 sales_data <- read.csv("C:\\Users\\delosh\\Documents\\Grad Year 1 Spring Semester 2017\\X-Informatics\\train.csv", header = TRUE)
 
 # user input data - assumed to be text 
-input_data <- read.table("filename", sep = "x")
+input_data <- read.table("C:\\Users\\delosh\\Documents\\Grad Year 1 Spring Semester 2017\\X-Informatics\\annotated.txt", sep = "~")
+
+
+# goals: sales graph generation
+# densities and dot plots based on statistics
+# number of customers, sales based on the week
+
+# get the variables based on input
+analysis_type <- as.character(input_data[1,1])
+var_of_interest <- as.character(input_data[2,1])
+store_vars <- as.character(input_data[3,1])
+store_lab <- as.character(input_data[4,1])
+time_lab <- as.character(input_data[5,1])
+plot_title <- as.character(input_data[6,1])
+y_axis_title <- as.character(input_data[7,1])
+time_unit <- as.character(input_data[8,1])
+
+# parse the stores
+if(store_vars != "all"){
+  store_vars <- as.numeric(unlist(strsplit(store_vars,",")))
+} else{
+  store_vars <- unique(sales_data[,store_lab])
+}
+
+#subset the data for what stores we want to look at
+sub.df <- subset(sales_data,(sales_data[,store_lab] %in% store_vars))
+sub.df[,store_lab] <- as.factor(sub.df[,store_lab])
 
 # exploratory analysis ----
 
-# goals: sales graph generation
-# bar charts and histograms based on statistics
-# number of customers, sales based on the week
 
-# multiple store graphs
-# strtoi(thing)
-
-mult_store_number <- c(1,2,3)
-store_var <- as.character("Store")
-var_of_interest_x <- as.character("Store")
-var_of_interest_y <- as.character("Sales")
-
-sub.df <- subset(sales_data,(sales_data[,var_of_interest_x] %in% mult_store_number))
-sub.df[,var_of_interest_x] <- as.factor(sub.df[,var_of_interest_x])
+if (analysis_type=="density"){
 
 # density plot
 # colors after decisions
+p <- ggplot(sub.df,aes_string(var_of_interest)) + 
+  geom_density(alpha = .1, aes_string(fill=store_lab, color = store_lab))+
+  ggtitle(plot_title)+
+  labs(x=y_axis_title,y="Density")+
+  theme(plot.title = element_text(hjust = 0.5))
 
-# density plot
-# colors after decisions
-p <- ggplot(sub.df,aes_string(var_of_interest_y)) + 
-  geom_density(alpha = .1, aes_string(fill=var_of_interest_x, color = var_of_interest_x))+
-  ggtitle("title here")+
-  labs(x="def",y="Density")
-
+t <- tidy(summary(fivenum(sub.df[,var_of_interest])))
+} else if(analysis_type=="dot"){
 # dot plot
 var_of_interest_x <- as.character("Date")
-p <- ggplot(sub.df,aes_string(var_of_interest_x,var_of_interest_y)) + 
-  geom_point(aes_string(fill=store_var, color = store_var)) +
+p <- ggplot(sub.df,aes_string(time_lab,var_of_interest)) + 
+  geom_point(aes_string(fill=store_lab, color = store_lab)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, size = .3))+
-  ggtitle("title here")+
-  labs(x="TimeUnit",y="VarofInterest")
+  ggtitle(plot_title)+
+  labs(x=time_unit,y=y_axis_title)+
+  theme(plot.title = element_text(hjust = 0.5))
 
 # text
 
-t <- tidy(summary(fivenum(sub.df[,"Sales"])))
-
+t <- tidy(summary(fivenum(sub.df[,var_of_interest])))
+} else {
 # sales regression ----
 
-time_var <- as.character("Date")
-times <- c(1:length(unique(sales_data[,time_var])))
-var_of_interest_x <- as.character("Store")
+times <- c(1:length(unique(sales_data[,time_lab])))
 
-# number of stores
 #do linear regression
-lin_reg <- lm(subset(sales_data,(sales_data[,var_of_interest_x] %in% 1))$Sales ~ times)
+lin_reg <- lm(subset(sales_data,(sales_data[,store_lab] %in% store_vars))[,var_of_interest] ~ times)
 coeff <- lin_reg$coefficients # resulting coefficients
 
+# resulting y values
 res <- coeff[1] + (coeff[2]*times)
 
-lr.df <- data.frame(times =times,date=unique(sales_data[,time_var]),original=sales_data[sales_data[,var_of_interest_x] %in% 1,"Sales"], result=res)
+# put results and original into easy to access dataframe
+lr.df <- data.frame(times =times,date=unique(sales_data[,time_lab]),original=sales_data[sales_data[,store_lab] %in% store_vars,var_of_interest], result=res)
 
+# linear regression plot
 p <- ggplot(lr.df) +
   geom_point(aes(times,original))+
-  geom_line(aes(times,result), size = 2, color="red", alpha = .5)+
-  ggtitle("x Regression")+
-  labs(x="TimeUnit",y="VariableofInterest")
+  geom_line(aes(times,result), size = 2, color="blue", alpha = .5)+
+  ggtitle(plot_title)+
+  labs(x=time_unit,y=var_of_interest)+
+  theme(plot.title = element_text(hjust = 0.5))
 
 # add text output
 t <- tidy(lin_reg)
+}
 
 # save output ----
 
 # save image
-png(filename = "filename_extension.png")
+png(filename = "Density.png", pointsize = 26,width = 620, height = 420)
 plot(p)
 dev.off()
 
 # save text
-write.table(t, file = "somehting.txt")
+write.table(t, file = "5numbersummary.txt")
