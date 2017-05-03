0a06ff4f9d03590188e368697717415abce0dd66
Spencer Whitehead <spencerwhitehead@Spencers-MacBook-Pro.local>
2017-05-02 21:51:15 -0400

    Commented code

--- app/file_upload.py
+++ app/file_upload.py
@@ -7,29 +7,39 @@ from werkzeug import secure_filename
 
 import app
 
+# Relevant directories
 UPLOAD_FOLDER = 'data/'
 RESULT_FOLDER = 'app/static/results/'
 
+# Valid file extensions
 ALLOWED_EXTENSIONS = set(['txt', 'csv'])
+
+# Fields from web interface
 FIELDS = ['mode', 'voi', 'stores', 'store_col',
             'time_col', 'title', 'yaxis', 'time_int']
+
+# Five number summary fields
 DENS_PLT_SUMMARY = ['minimum', 'maximum', 'median',
             'mean', 'q1', 'q3']
 
+# Demo mode
 DEMO = True
 
 f_uploader = Blueprint('file_upload', __name__)
 
+# Determine if file has valid extension
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+# Order configuration fields
 def order_config(c, fields):
     config = []
     for f in fields:
         config.append(c[f])
     return config
 
+# Write input to configuration file
 def write_config(fname, entries, fields):
     data = order_config(entries, fields)
     with open(fname, 'w') as f:
@@ -37,6 +47,7 @@ def write_config(fname, entries, fields):
             f.write(d)
             f.write('\n')
 
+# Read five number summary from R script
 def read_num_summary(fname):
     with open(fname, 'r') as f:
         count = 0
@@ -53,13 +64,15 @@ def read_num_summary(fname):
             count += 1
     return dict(zip(keys, vals))
 
-def get_num_sum_str(fname):
+# Return list of five number summary
+def get_num_sum(fname):
     results = read_num_summary(fname)
     output = []
     for num in DENS_PLT_SUMMARY:
         output.append('%s: %s' % (num, results[num]))
     return output
 
+# Read regression summary from R script
 def read_reg_summary(fname):
     with open(fname, 'r') as f:
         count = 0
@@ -75,13 +88,15 @@ def read_reg_summary(fname):
             count += 1
         return all_lines
 
-def get_reg_str(fname):
+# Return list of regression summary
+def get_reg_sum(fname):
     results = read_reg_summary(fname)
     output = []
     for r in results:
         output.append(' '.join(r))
     return output
 
+# Handle incoming requests
 @f_uploader.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -90,12 +105,13 @@ def index():
             return redirect(request.url)
         file = request.files['file']
 
-        # if user does not select file, browser also
+        # If user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
 
+        # Check if file is valid and save configuration
         config = ''
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
@@ -106,18 +122,18 @@ def index():
         if not DEMO:
             subprocess.call (['/usr/bin/Rscript', '--vanilla', 'MyrScript.r', config])
             output_name = RESULT_FOLDER+filename
-            result_text = get_reg_str(output_name+'.results.txt')
+            result_text = get_reg_sum(output_name+'.results.txt')
             return render_template('index.html',
                 result_image=output_name+'.results.png',
                 result_text=result_text)
         else:
             if request.form['mode'] == 'reg':
-                result_text = get_reg_str(RESULT_FOLDER+'Regression.txt')
+                result_text = get_reg_sum(RESULT_FOLDER+'Regression.txt')
                 return render_template('index.html',
                     result_image='/static/image/Regression.png',
                     result_text=result_text)
             else:
-                result_text = get_num_sum_str(RESULT_FOLDER+'5numbersummary.txt')
+                result_text = get_num_sum(RESULT_FOLDER+'5numbersummary.txt')
                 if request.form['mode'] == 'den':
                     return render_template('index.html',
                             result_image='/static/image/Density.png',
@@ -128,11 +144,13 @@ def index():
                             result_text=result_text)
 
     elif request.method == 'GET':
+        # If download
         if 'dl' in request.args:
             return result_download(request.args['dl'])
         else:
             return render_template('index.html')
 
+# Download result plot and summary
 def result_download(fname):
     if not fname:
         raise InvalidUsage("", status_code=400)
